19eeb902e9cf1b7f68efce4f3705e2f22ec06105
Arnaud Héritier <aheritier@exoplatform.com>
2014-06-17 14:24:33 +0200

    Make tests more self-explanatory (even if it covers less combinations)

--- src/test/groovy/org/exoplatform/platform/am/cli/CommandLineParserTest.groovy
+++ src/test/groovy/org/exoplatform/platform/am/cli/CommandLineParserTest.groovy
@@ -63,9 +63,24 @@ class CommandLineParserTest extends Specification {
     !cliArgs.commandList.snapshots
     where:
     args << [
-        ["--verbose", "-v", ""],
         ["list"],
-    ].combinations().collect { it.minus("") }
+    ]
+  }
+
+  def "Test command line parameters to list all add-ons with verbose logs"(String[] args) {
+    when:
+    CommandLineParameters cliArgs = clp.parse(args)
+    then:
+    CommandLineParameters.Command.LIST == cliArgs.command
+    !cliArgs.commandList.snapshots
+    cliArgs.verbose
+    where:
+    args << [
+        ["list", "-v"],
+        ["list", "--verbose"],
+        ["-v", "list"],
+        ["--verbose", "list"],
+    ]
   }
 
   def "Test command line parameters to list all add-ons including snapshots"(String[] args) {
@@ -76,10 +91,29 @@ class CommandLineParserTest extends Specification {
     cliArgs.commandList.snapshots
     where:
     args << [
-        ["--verbose", "-v", ""],
-        ["list"],
-        ["--snapshots", "-s"],
-    ].combinations().collect { it.minus("") }
+        ["list", "--snapshots"],
+        ["list", "-s"],
+    ]
+  }
+
+  def "Test command line parameters to list all add-ons including snapshots with verbose logs"(String[] args) {
+    when:
+    CommandLineParameters cliArgs = clp.parse(args)
+    then:
+    CommandLineParameters.Command.LIST == cliArgs.command
+    cliArgs.commandList.snapshots
+    cliArgs.verbose
+    where:
+    args << [
+        ["list", "-v", "--snapshots"],
+        ["list", "--verbose", "--snapshots"],
+        ["-v", "list", "--snapshots"],
+        ["--verbose", "list", "--snapshots"],
+        ["list", "-v", "-s"],
+        ["list", "--verbose", "-s"],
+        ["-v", "list", "-s"],
+        ["--verbose", "list", "-s"],
+    ]
   }
 
   def "Test command line parameters to list add-ons with a valid catalog parameter"(String[] args) {
@@ -90,10 +124,8 @@ class CommandLineParserTest extends Specification {
     validCatalogUrl.equals(cliArgs.commandList.catalog.toString())
     where:
     args << [
-        ["--verbose", "-v", ""],
-        ["list"],
-        ["--catalog=${validCatalogUrl}"],
-    ].combinations().collect { it.minus("") }
+        ["list", "--catalog=${validCatalogUrl}"],
+    ]
   }
 
   def "Test command line parameters to list add-ons with a invalid catalog parameter"(String[] args) {
@@ -103,10 +135,8 @@ class CommandLineParserTest extends Specification {
     thrown(CommandLineParsingException)
     where:
     args << [
-        ["--verbose", "-v", ""],
-        ["list"],
-        ["--catalog=${invalidCatalogUrl}"],
-    ].combinations().collect { it.minus("") }
+        ["list", "--catalog=${invalidCatalogUrl}"],
+    ]
   }
 
   def "Test command line parameters to install the latest version of an add-on"(String[] args) {
@@ -120,10 +150,8 @@ class CommandLineParserTest extends Specification {
     cliArgs.commandInstall.addonVersion == null
     where:
     args << [
-        ["--verbose", "-v", ""],
-        ["install"],
-        ["my-addon"],
-    ].combinations().collect { it.minus("") }
+        ["install", "my-addon"],
+    ]
   }
 
   def "Test command line parameters to install a given version of an add-on"(String[] args) {
@@ -137,10 +165,8 @@ class CommandLineParserTest extends Specification {
     "42".equals(cliArgs.commandInstall.addonVersion)
     where:
     args << [
-        ["--verbose", "-v", ""],
-        ["install"],
-        ["my-addon:42"],
-    ].combinations().collect { it.minus("") }
+        ["install", "my-addon:42"],
+    ]
   }
 
   def "Test command line parameters to force to install an add-on"(String[] args) {
@@ -153,11 +179,11 @@ class CommandLineParserTest extends Specification {
     "my-addon".equals(cliArgs.commandInstall.addonId)
     where:
     args << [
-        ["--verbose", "-v", ""],
-        ["install"],
-        ["my-addon", "my-addon:42"],
-        ["--force", "-f"],
-    ].combinations().collect { it.minus("") }
+        ["install", "my-addon", "--force"],
+        ["install", "my-addon:42", "--force"],
+        ["install", "my-addon", "-f"],
+        ["install", "my-addon:42", "-f"],
+    ]
   }
 
   def "Test command line parameters to install a SNAPSHOT version of an add-on"(String[] args) {
@@ -170,11 +196,11 @@ class CommandLineParserTest extends Specification {
     "my-addon".equals(cliArgs.commandInstall.addonId)
     where:
     args << [
-        ["--verbose", "-v", ""],
-        ["install"],
-        ["my-addon", "my-addon:42-SNAPSHOT"],
-        ["--snapshots", "-s"],
-    ].combinations().collect { it.minus("") }
+        ["install", "my-addon", "--snapshots"],
+        ["install", "my-addon:42-SNAPSHOT", "--snapshots"],
+        ["install", "my-addon", "-s"],
+        ["install", "my-addon:42-SNAPSHOT", "-s"],
+    ]
   }
 
   def "Test command line parameters to install an add-on with a valid catalog parameter"(String[] args) {
@@ -185,11 +211,9 @@ class CommandLineParserTest extends Specification {
     validCatalogUrl.equals(cliArgs.commandInstall.catalog.toString())
     where:
     args << [
-        ["--verbose", "-v", ""],
-        ["install"],
-        ["my-addon", "my-addon:42"],
-        ["--catalog=${validCatalogUrl}"],
-    ].combinations().collect { it.minus("") }
+        ["install", "my-addon", "--catalog=${validCatalogUrl}"],
+        ["install", "my-addon:42", "--catalog=${validCatalogUrl}"],
+    ]
   }
 
   def "Test command line parameters to install an add-on with a invalid catalog parameter"(String[] args) {
@@ -199,11 +223,9 @@ class CommandLineParserTest extends Specification {
     thrown(CommandLineParsingException)
     where:
     args << [
-        ["--verbose", "-v", ""],
-        ["install"],
-        ["my-addon", "my-addon:42"],
-        ["--catalog=${invalidCatalogUrl}"],
-    ].combinations().collect { it.minus("") }
+        ["install", "my-addon", "--catalog=${invalidCatalogUrl}"],
+        ["install", "my-addon:42", "--catalog=${invalidCatalogUrl}"],
+    ]
   }
 
   def "Test command line parameters to uninstall an add-on"(String[] args) {
@@ -214,10 +236,8 @@ class CommandLineParserTest extends Specification {
     "my-addon".equals(cliArgs.commandUninstall.addonId)
     where:
     args << [
-        ["--verbose", "-v", ""],
-        ["uninstall"],
-        ["my-addon"],
-    ].combinations().collect { it.minus("") }
+        ["uninstall","my-addon"],
+    ]
   }
 
   def "Test invalid command line parameters"(String[] args) {
