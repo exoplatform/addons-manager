0970ad88054abafb640a3e6d8b9029e04872748b
Arnaud Héritier <aheritier@exoplatform.com>
2014-06-18 20:41:20 +0200

    Use an embedded web server to server data (catalog + add ons archives) to improve the reliability of tests and to be able to add more tests without using any production data

--- src/test/groovy/org/exoplatform/platform/am/AddonsManagerIT.groovy
+++ src/test/groovy/org/exoplatform/platform/am/AddonsManagerIT.groovy
@@ -20,6 +20,8 @@
  */
 package org.exoplatform.platform.am
 
+import org.apache.catalina.startup.Tomcat
+import org.exoplatform.platform.am.settings.AddonsManagerSettings
 import org.exoplatform.platform.am.settings.PlatformSettings
 import spock.lang.Shared
 import spock.lang.Specification
@@ -38,6 +40,8 @@ class AddonsManagerIT extends Specification {
   String testDataPath = System.getProperty("testDataPath")
   @Shared
   File productHome = new File(System.getProperty("integrationTestsDirPath")).listFiles().first()
+  @Shared
+  Tomcat tomcat = new Tomcat();
 
   def setupSpec() {
     assertNotNull("Tested artifact path mustn't be null", testedArtifactPath)
@@ -46,6 +50,15 @@ class AddonsManagerIT extends Specification {
     assertTrue("Integration tests directory must be a directory",
                new File(System.getProperty("integrationTestsDirPath")).isDirectory())
     assertTrue("PLF_HOME must be a directory", productHome.isDirectory())
+    // Let's start a web server to serve test data
+    tomcat.setPort(Integer.getInteger("tomcatHttpPort"));
+    tomcat.addWebapp("/", new File(testDataPath).getAbsolutePath());
+    tomcat.start();
+  }
+
+  def cleanupSpec() {
+    // Let's stop the web server
+    tomcat.stop()
   }
 
   def "Test exit code"(String params, int expectedExitCode) {
@@ -54,27 +67,27 @@ class AddonsManagerIT extends Specification {
     expectedExitCode == launchAddonsManager(params)
 
     where:
-    params                                                                     | expectedExitCode
-    ""                                                                         | AddonsManagerConstants.RETURN_CODE_INVALID_COMMAND_LINE_PARAMS // Without any param the program must return an error code 1
-    "--help"                                                                   | AddonsManagerConstants.RETURN_CODE_OK // With --help param the program must display the help return 0
-    "list"                                                                     | AddonsManagerConstants.RETURN_CODE_OK // With list param the program must display the list of available add-ons and return 0
-    "list --catalog=file://${System.getProperty("testDataPath")}/catalog.json" | AddonsManagerConstants.RETURN_CODE_OK // List add-ons from another catalog [AM_CAT_02]
-    "list --no-cache"                                                          | AddonsManagerConstants.RETURN_CODE_OK // List add-ons without using local cache
-    "list --offline"                                                           | AddonsManagerConstants.RETURN_CODE_OK // List add-ons in offline mode (thus from data in cache)
-    "list --no-cache --offline"                                                | AddonsManagerConstants.RETURN_CODE_OK // List add-ons in offline mode with no cache (thus only the local catalog is used)
-    "install exo-chat-extension"                                               | AddonsManagerConstants.RETURN_CODE_OK // Install an extension
-    "uninstall exo-chat-extension"                                             | AddonsManagerConstants.RETURN_CODE_OK // Uninstall an extension
-    "list --snapshots"                                                         | AddonsManagerConstants.RETURN_CODE_OK // With list --snapshots param the program must display the list of available add-ons and return 0
-    "install exo-sirona:1.0.0"                                                 | AddonsManagerConstants.RETURN_CODE_OK // Install another extension with a given version
-    "install exo-sirona --snapshots"                                           | AddonsManagerConstants.RETURN_CODE_ADDON_ALREADY_INSTALLED // Install the same extension must fail
-    "install exo-sirona --snapshots --force"                                   | AddonsManagerConstants.RETURN_CODE_OK // Install the same extension must succeed if forced
-    "uninstall exo-sirona"                                                     | AddonsManagerConstants.RETURN_CODE_OK // Uninstall it
-    "install exo-sirona --no-cache"                                            | AddonsManagerConstants.RETURN_CODE_OK // Install the same extension without cache must succeed
-    "uninstall exo-sirona"                                                     | AddonsManagerConstants.RETURN_CODE_OK // Uninstall it
-    "install exo-sirona --offline"                                             | AddonsManagerConstants.RETURN_CODE_OK // Install the same extension in offline mode must succeed
-    "uninstall exo-sirona"                                                     | AddonsManagerConstants.RETURN_CODE_OK // Uninstall it
-    "install unknown-addon"                                                    | AddonsManagerConstants.RETURN_CODE_ADDON_NOT_FOUND // Install unknown add-on
-    "uninstall unknown-addon"                                                  | AddonsManagerConstants.RETURN_CODE_ADDON_NOT_INSTALLED // Uninstall unknown add-on
+    params                                                                       | expectedExitCode
+    ""                                                                           | AddonsManagerConstants.RETURN_CODE_INVALID_COMMAND_LINE_PARAMS // Without any param the program must return an error code 1
+    "--help"                                                                     | AddonsManagerConstants.RETURN_CODE_OK // With --help param the program must display the help return 0
+    "list"                                                                       | AddonsManagerConstants.RETURN_CODE_OK // With list param the program must display the list of available add-ons and return 0
+    "list --catalog=http://localhost:${tomcat.connector.localPort}/catalog.json" | AddonsManagerConstants.RETURN_CODE_OK // List add-ons from another catalog [AM_CAT_02]
+    "list --no-cache"                                                            | AddonsManagerConstants.RETURN_CODE_OK // List add-ons without using local cache
+    "list --offline"                                                             | AddonsManagerConstants.RETURN_CODE_OK // List add-ons in offline mode (thus from data in cache)
+    "list --no-cache --offline"                                                  | AddonsManagerConstants.RETURN_CODE_OK // List add-ons in offline mode with no cache (thus only the local catalog is used)
+    "install test-addon"                                                         | AddonsManagerConstants.RETURN_CODE_OK // Install an add-on
+    "uninstall test-addon"                                                       | AddonsManagerConstants.RETURN_CODE_OK // Uninstall an add-on
+    "list --snapshots"                                                           | AddonsManagerConstants.RETURN_CODE_OK // With list --snapshots param the program must display the list of available add-ons and return 0
+    "install test-addon:1.0.0"                                                   | AddonsManagerConstants.RETURN_CODE_OK // Install another extension with a given version
+    "install test-addon --snapshots"                                             | AddonsManagerConstants.RETURN_CODE_ADDON_ALREADY_INSTALLED // Install the same extension must fail
+    "install test-addon --snapshots --force"                                     | AddonsManagerConstants.RETURN_CODE_OK // Install the same extension must succeed if forced
+    "uninstall test-addon"                                                       | AddonsManagerConstants.RETURN_CODE_OK // Uninstall it
+    "install test-addon --no-cache"                                              | AddonsManagerConstants.RETURN_CODE_OK // Install the same extension without cache must succeed
+    "uninstall test-addon"                                                       | AddonsManagerConstants.RETURN_CODE_OK // Uninstall it
+    "install test-addon --offline"                                               | AddonsManagerConstants.RETURN_CODE_OK // Install the same extension in offline mode must succeed
+    "uninstall test-addon"                                                       | AddonsManagerConstants.RETURN_CODE_OK // Uninstall it
+    "install unknown-addon"                                                      | AddonsManagerConstants.RETURN_CODE_ADDON_NOT_FOUND // Install unknown add-on
+    "uninstall unknown-addon"                                                    | AddonsManagerConstants.RETURN_CODE_ADDON_NOT_INSTALLED // Uninstall unknown add-on
 
   }
 
@@ -90,6 +103,7 @@ class AddonsManagerIT extends Specification {
       commandToExecute << "${System.getProperty('jacocoAgent')}"
     }
     commandToExecute << "-D${PlatformSettings.PLATFORM_HOME_SYS_PROP}=${productHome.absolutePath}"
+    commandToExecute << "-D${AddonsManagerSettings.PROPERTY_PREFIX}.centralCatalogUrl=http://localhost:${tomcat.connector.localPort}/catalog.json"
     commandToExecute << "-jar ${testedArtifactPath}"
     commandToExecute << params
     println "Command launched : ${commandToExecute.join(' ')}"
